47aebc64b93f8cc13ad330abad61803a4ce1d7ec
tomatereloco <tomascllc@hotmail.com>
2013-11-02 00:55:58 -0200

    FC

---
+++ src/model/element/Wall.java
@@ -0,0 +1,18 @@
+package model.element;
+
+import model.board.Content;
+import model.fighter.Fighter;
+
+public class Wall implements Content{
+
+	@Override
+	public boolean canWalkOver() {
+		return false;
+	}
+
+	@Override
+	public Content interact(Fighter hero) {
+		return null;
+	}
+
+}
