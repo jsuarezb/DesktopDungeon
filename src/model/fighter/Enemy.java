c541aa6cfc45d06d60aaf7f201e0a4d8ca41e07e
agustinmounier <agusmouni@gmail.com>
2013-11-02 02:53:15 -0200

    Enemy commit 2

--- src/model/fighter/Enemy.java
+++ src/model/fighter/Enemy.java
@@ -5,10 +5,20 @@ import model.fighter.level.Level;
 
 public abstract class Enemy extends Fighter {
 	
+	protected double F;
+	protected double S;
+	
+	public Enemy(double S, double F){
+		this.F = F;
+		this.S = S;
+	}
+	
 	@Override
 	public boolean canWalkOver() {
 		return false;
 	}
+	
+	
 
 	@Override
 	public Content interact(Fighter hero) {
@@ -39,17 +49,14 @@ public abstract class Enemy extends Fighter {
 	
 	@Override
 	public int getMaxHealth() {
-		return (int) Math.floor(((level.getValue() + 3)^2 - 10)*getS());
+		return (int) Math.floor(((level.getValue() + 3)^2 - 10)*S);
 	}
 	
 	@Override
 	public int getStrength() {
-		return (int) Math.floor(((level.getValue())^2 + 5*level.getValue())*0.5*getF());
+		return (int) Math.floor(((level.getValue())^2 + 5*level.getValue())*0.5*F);
 	}
 	
 	public abstract Content drop();	
 	
-	public abstract double getS();	
-	
-	public abstract double getF();	
 }
