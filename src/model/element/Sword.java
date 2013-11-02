2ae167d95d9e18b6bab0fa4accb5f77d3b51d5d7
tomatereloco <tomascllc@hotmail.com>
2013-11-02 00:25:32 -0200

    Valuable

--- src/model/element/Sword.java
+++ src/model/element/Sword.java
@@ -4,7 +4,7 @@ import model.board.Content;
 import model.fighter.Fighter;
 import model.fighter.Hero;
 
-public class Sword extends Item{
+public class Sword extends ValuableItem{
 	
 	public Sword(int value){
 		super(value);
