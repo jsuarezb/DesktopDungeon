e546101ecda55c117234bb1e659191bc26ca2c5f
tomatereloco <tomascllc@hotmail.com>
2013-11-02 02:07:09 -0200

    goblin F y S

--- src/model/fighter/Goblin.java
+++ src/model/fighter/Goblin.java
@@ -2,16 +2,26 @@ package model.fighter;
 
 import model.board.Content;
 import model.element.Blood;
+package model.fighter;
+
+import model.board.Content;
+import model.element.Blood;
 
 public class Goblin extends Enemy{
 	
-	public Goblin(double s, double f) {
-		super(s, f);
-	}
+	private final static double S = 1;
+	private final static double F = 0.7;
 	
 	@Override
 	public Content drop() {
 		return new Blood();
 	}
-}
 
+	public double getF() {
+		return F;
+	}
+
+	public double getS() {
+		return S;
+	}
+}
