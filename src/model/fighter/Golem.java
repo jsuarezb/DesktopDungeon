d3ece6300f719b6e2ae0835235374a9bae76278b
tomatereloco <tomascllc@hotmail.com>
2013-11-02 02:07:29 -0200

    F y S golem

--- src/model/fighter/Golem.java
+++ src/model/fighter/Golem.java
@@ -5,13 +5,19 @@ import model.element.Sword;
 
 public class Golem extends Enemy{
 
-	public Golem(double s, double f) {
-		super(s, f);
-	}
+	private final static double S = 1.35;
+	private final static double F = 1;
 	
 	@Override
 	public Content drop() {
 		return new Sword(getLevel().getValue());
 	}
-}
 
+	public double getF() {
+		return F;
+	}
+
+	public double getS() {
+		return S;
+	}
+}
