38812ae8087febf40c7a5934832066682b6ebe9e
agustinmounier <agusmouni@gmail.com>
2013-11-02 02:53:59 -0200

    goblin commit 2

--- src/model/fighter/Goblin.java
+++ src/model/fighter/Goblin.java
@@ -9,19 +9,16 @@ import model.element.Blood;
 
 public class Goblin extends Enemy{
 	
-	private final static double S = 1;
-	private final static double F = 0.7;
+	public final static double S = 1;
+	public final static double F = 0.7;
+	
+	public Goblin(){
+		super(S, F);
+	}
 	
 	@Override
 	public Content drop() {
 		return new Blood();
 	}
 
-	public double getF() {
-		return F;
-	}
-
-	public double getS() {
-		return S;
-	}
 }
