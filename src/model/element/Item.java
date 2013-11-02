1336ba7ba0cd2ce0ee6f2abd1f228ed3406b0214
tomatereloco <tomascllc@hotmail.com>
2013-11-02 00:20:05 -0200

    Item pasa a ser mas generico

--- src/model/element/Item.java
+++ src/model/element/Item.java
@@ -1,20 +1,9 @@
 package model.element;
 
-public abstract class Item implements Valuable {
-
-	private int value;
-	
-	public Item(int value){
-		this.value = value;
-	}
+public abstract class Item extends Content{
 	
 	@Override
 	public boolean canWalkOver() {
 		return true;
-	}
-	
-	@Override
-	public int getValue() {
-		return value;
 	}
 }
